5cd847229b6d0de285acadcbf2a78ddb366862b1
grainier <ivone.perera@gmail.com>
2013-04-24 15:51:25 +0530

    Bootstrap configured

--- index.php
+++ index.php
@@ -2,7 +2,10 @@
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title></title>
+        <title>HCI-CW2</title>
+        <link href="css/bootstrap.css" rel="stylesheet">
+        <link href="css/bootstrap-responsive.css" rel="stylesheet">
+        <script src="js/bootstrap.js"></script>
     </head>
     <body>
         <?php
